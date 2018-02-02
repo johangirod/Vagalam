252b4d93874fec0255ff4be22e03919390c151f5
Johan Girod <johangirod@gmail.com>
2018-02-02 12:29:14 +0100

    feat: add dots to navigate between pictures & better keyboard navigation

--- src/Trip/Posts/Pictures/index.jsx
+++ src/Trip/Posts/Pictures/index.jsx
@@ -4,16 +4,26 @@ import { withStyles } from 'vitaminjs';
 import classnames from 'classnames';
 import { CSSTransitionGroup } from 'react-transition-group';
 import s from './style.css';
-
-class Pictures extends Component {
+import type { Picture } from '../types';
+type Props = {
+    pictures: Array<Picture>,
+    isFullscreen: boolean,
+};
+type State = {
+    currentPictureIndex: number,
+    previewIndex: ?number,
+};
+class Pictures extends Component<Props, State> {
     constructor(props) {
         super(props);
         this.state = {
             currentPictureIndex: 0,
+            previewIndex: null,
         };
     }
     componentDidMount() {
         this.resetInterval();
+        this.pictureDiv.focus();
     }
     resetInterval = () => {
         if (this.intervalId) {
@@ -25,23 +35,77 @@ class Pictures extends Component {
         this.setState(({ currentPictureIndex }) => ({
             currentPictureIndex: (currentPictureIndex + 1) % this.props.pictures.length,
         }));
+        this.resetInterval();
+    };
+    goToPreviousPicture = () => {
+        this.setState(({ currentPictureIndex }) => ({
+            currentPictureIndex:
+                (this.props.pictures.length + currentPictureIndex - 1) % this.props.pictures.length,
+        }));
+        this.resetInterval();
     };
     handleClickOnPicture = () => {
         if (!this.props.isFullscreen) {
             return;
         }
         this.goToNextPicture();
-        this.resetInterval();
     };
+    handleClickOnDot = (e: MouseEvent, i: Number) => {
+        this.setState({ currentPictureIndex: i });
+        if (this.props.isFullscreen) {
+            e.stopPropagation();
+        }
+    };
+    handleMouseEnterDot = (i: Number) => {
+        this.setState({
+            previewIndex: i,
+        });
+    };
+    handleMouseLeaveDot = () => {
+        this.setState({
+            previewIndex: null,
+        });
+    };
+    handleKeyDown = (e: KeyboardEvent) => {
+        if (
+            e.key === ' ' &&
+            (!this.props.isFullscreen ||
+                this.state.currentPictureIndex === this.props.pictures.length - 1)
+        ) {
+            return;
+        }
+        if (e.key === ' ' || e.key === 'ArrowRight') {
+            this.goToNextPicture();
+            e.stopPropagation();
+        }
+        if (e.key === 'ArrowLeft') {
+            this.goToPreviousPicture();
+            e.stopPropagation();
+        }
+    };
+
     render() {
         const { pictures } = this.props;
-        const currentPicture = pictures[this.state.currentPictureIndex];
+        const currentPictureIndex =
+            this.state.previewIndex !== null
+                ? this.state.previewIndex
+                : this.state.currentPictureIndex;
+        const currentPicture = pictures[currentPictureIndex];
         return (
-            <div className={s.pictures} onClick={this.handleClickOnPicture}>
+            <div
+                role="button"
+                className={s.pictures}
+                onClick={this.handleClickOnPicture}
+                onKeyDown={this.handleKeyDown}
+                tabIndex={0}
+                ref={ref => {
+                    this.pictureDiv = ref;
+                }}
+            >
                 {pictures.map((picture, i) => (
                     <div
                         className={classnames(s['picture-container'], {
-                            [s.show]: this.state.currentPictureIndex === i,
+                            [s.show]: currentPictureIndex === i,
                             [s.unique]: pictures.length === 1,
                         })}
                         key={picture.url}
@@ -71,6 +135,25 @@ class Pictures extends Component {
                         </div>
                     </CSSTransitionGroup>
                 ) : null}
+                {pictures.length > 1 && (
+                    <div className={s['side-dots']}>
+                        {pictures.map((picture, i) => (
+                            <button
+                                className={classnames(s.dot, {
+                                    [s.selected]: currentPictureIndex === i,
+                                })}
+                                onClick={e => this.handleClickOnDot(e, i)}
+                                key={picture.url}
+                                onMouseEnter={() => this.handleMouseEnterDot(i)}
+                                onMouseLeave={this.handleMouseLeaveDot}
+                            >
+                                <div role="img" aria-label="dot">
+                                    ●
+                                </div>
+                            </button>
+                        ))}
+                    </div>
+                )}
             </div>
         );
     }
